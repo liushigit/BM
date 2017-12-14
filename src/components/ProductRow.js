eba5795fa818f005d5e7515802ed77625c6fe0c2
Shi Liu <liushie@hotmail.com>
2017-12-14 09:24:26 +0800

    ProductRow style changes

--- src/components/ProductRow.js
+++ src/components/ProductRow.js
@@ -1,18 +1,31 @@
 // @flow
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 type Props = {
   name: string,
   price: string
 }
 
+const ROW_HEIGHT = 56
+
+const styles = StyleSheet.create({
+  rowText: { height: ROW_HEIGHT, lineHeight: ROW_HEIGHT }
+})
+
 export default class ProductRow extends Component<Props> {
   render() {
     return (
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Text style={{ backgroundColor: 'red' }}> {this.props.name} </Text>
-        <Text style={{ backgroundColor: 'blue' }}> {this.props.price} </Text>
+      <View
+        style={{
+          height: ROW_HEIGHT,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <Text> {this.props.name} </Text>
+        <Text> {this.props.price} </Text>
       </View>
     )
   }
